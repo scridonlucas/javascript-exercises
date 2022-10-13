8f4bf9c34fe287f3a9512eaeebe5f32004f8c4f6
scridonlucas <lukas_scridon@yahoo.com>
2022-10-13 18:17:54 +0300

    rezolvare

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const myArray = text.split("");
+    let newText = "";
+    for(let i = myArray.length - 1; i >=0; i--) {
+        newText += myArray[i];
+    }
+    return newText;
 };
+
+console.log(reverseString("abc"));
 
 // Do not edit below this line
 module.exports = reverseString;
