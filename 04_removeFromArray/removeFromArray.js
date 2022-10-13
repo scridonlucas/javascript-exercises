bab750fb50ad9cfa3867b87c743b491941cbead3
scridonlucas <lukas_scridon@yahoo.com>
2022-10-13 20:27:00 +0300

    Final

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,13 @@
-const removeFromArray = function(myArray, removeElement) {
+const removeFromArray = function(myArray, ...theArgs) {
     for(let i = 0; i < myArray.length; i++) {
-        if(myArray[i] === removeElement) {
+        if(theArgs.includes(myArray[i])) {
             myArray.splice(i, 1);
+            i--;
         }
     }
     return myArray;
 };
 
 // Do not edit below this line
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 module.exports = removeFromArray;
