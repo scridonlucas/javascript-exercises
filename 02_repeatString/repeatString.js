0c7a676ae550a8de017d8c3858e41f63c245a891
scridonlucas <lukas_scridon@yahoo.com>
2022-10-13 17:16:53 +0300

    .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let returnedText = "";
+    for(let i = 0; i < number; i++) {
+        returnedText += text;
+    }
+    return returnedText;
 };
 
 // Do not edit below this line
