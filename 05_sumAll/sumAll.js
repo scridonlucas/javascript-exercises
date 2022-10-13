e9a9a4256d73d8983cad5382593cb3403a1af012
scridonlucas <lukas_scridon@yahoo.com>
2022-10-13 22:16:57 +0300

    final

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let finalSum = 0;
+    if(number1 < 0 || number2 < 0) return "ERROR";
+    if(typeof number1 != 'number' || typeof number2 != 'number') return "ERROR";
+    if(number1 > number2) {
+        let aux = number1;
+        number1 = number2;
+        number2 = aux;
+    }
+    for(let i = number1; i <= number2; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 
 };
 
