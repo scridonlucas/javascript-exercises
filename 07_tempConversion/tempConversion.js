d113f5c7338cca8cc7cdabc71509f5375ab0b503
scridonlucas <lukas_scridon@yahoo.com>
2022-10-14 13:52:11 +0300

    m

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  return Math.round((x-32) * (5/9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  return Math.round((x*9/5 + 32) * 10) / 10;
 };
+
+console.log(ftoc(100));
 
 // Do not edit below this line
 module.exports = {
