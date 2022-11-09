10cbbc42848c0ee95f148e52aeab39f64006afac
scridonlucas <lukas_scridon@yahoo.com>
2022-11-09 22:22:49 +0200

    rezolvare

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, element) => {
+    return total += element;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+  let product  = 1;
+
+	for(let i = a; i > 0; i--) {
+    product *= a;
+  }
 };
 
 // Do not edit below this line
