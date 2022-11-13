98f1587547d793264325483943125d18ed98b6b5
scridonlucas <lukas_scridon@yahoo.com>
2022-11-13 04:44:47 +0200

    finalizat

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z]/g, "");
+    let arrayString= newString.split("");
+    arrayString.reverse();
+    let inversedString = arrayString.join("");
+    return newString === inversedString;
 };
 
+console.log(palindromes("dad"));
 // Do not edit below this line
 module.exports = palindromes;
