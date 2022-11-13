913b8ff30d194761ff5a07a0d85688a48d8f2258
scridonlucas <lukas_scridon@yahoo.com>
2022-11-13 20:18:38 +0200

    final

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0) return "OOPS";
+    let a = [];
+    a[0] = 1;
+    a[1] = 1;
+    for(let i = 2; i < number; i++) {
+        a[i] = a[i-1] + a[i-2];
+    }
+    return a[number-1];
 };
 
 // Do not edit below this line
