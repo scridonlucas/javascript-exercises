d631374ba69ae2e925b2f5cd3c500b0ba3311e5c
scridonlucas <lukas_scridon@yahoo.com>
2022-11-16 00:48:58 +0200

    finalizare

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if(oldestAge < currentAge) {
+            return currentPerson;
+        } else return oldest;
+    })
+    function getAge(birth, death) {
+        if(!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
 
 };
 
